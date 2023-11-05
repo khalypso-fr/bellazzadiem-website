487f15e168d28f9ae055661248f1c192f46491cd
Mathieu KHALEM <mathieu.khalem@gmail.com>
2023-11-05 20:33:49 +0100

    Revert "Compress website resources"
    This reverts commit 169ba4644347c8395a69120b35890e37e9231bdb.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
 
-import compress from "astro-compress";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.bellezzadiem.fr",
-  integrations: [tailwind(), sitemap(), compress()]
+  integrations: [tailwind(), sitemap()]
 });
