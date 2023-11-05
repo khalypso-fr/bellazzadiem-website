169ba4644347c8395a69120b35890e37e9231bdb
Mathieu KHALEM <mathieu.khalem@gmail.com>
2023-11-05 20:31:28 +0100

    Compress website resources

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.bellezzadiem.fr",
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap(), compress()]
 });
