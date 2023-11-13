b6ce7cf9f49e7fac4c50c2931cbf84fde702dc30
Mathieu KHALEM <mathieu.khalem@gmail.com>
2023-11-13 23:31:34 +0100

    Add netlify integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import nodejs from "@astrojs/node";
 import sitemap from "@astrojs/sitemap";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.bellezzadiem.fr",
   output: "server",
-  adapter: nodejs({
-    mode: "standalone",
-  }),
+  adapter: netlify(),
   integrations: [tailwind(), sitemap()],
 });
