c95619ca3987acc708c0e8558d6d7011e0b32749
Mathieu KHALEM <mathieu.khalem@gmail.com>
2023-11-13 22:09:07 +0100

    Add contact page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
+import nodejs from "@astrojs/node";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.bellezzadiem.fr",
-  integrations: [tailwind(), sitemap()]
+  output: "server",
+  adapter: nodejs({
+    mode: "standalone",
+  }),
+  integrations: [tailwind(), sitemap()],
 });
