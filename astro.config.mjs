57e50f8edc11adac079c3d4c961949fb98fd46d2
Mathieu KHALEM <mathieu.khalem@gmail.com>
2024-02-07 23:50:08 +0100

    Update the configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
-import netlify from "@astrojs/netlify/functions";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
