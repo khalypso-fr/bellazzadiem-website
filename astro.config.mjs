77072e9aadf39d44ff5eac24d414582bffda0d4f
Mathieu KHALEM <mkhalem@directskills.com>
2025-04-02 11:30:19 +0200

    Upgrade all the dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
-import icon from "astro-icon";
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.bellezzadiem.fr",
-  output: "server",
+  output: "static",
   adapter: netlify(),
-  integrations: [icon(), tailwind(), sitemap()],
+  integrations: [sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
